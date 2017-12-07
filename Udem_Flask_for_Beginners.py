257edb6259921db35aa9620c53d83a6044798773
Ben Castan <ben@castanfamily.com>
2017-12-07 17:41:36 +1100

    Finished and tidying up

--- Udem_Flask_for_Beginners.py
+++ Udem_Flask_for_Beginners.py
@@ -9,14 +9,18 @@ def hello_world():
     bmi = 0.0
     if request.method == 'POST' and 'username' in request.form:
         name = request.form.get('username')
-        weight = request.form.get('weight')
-        height = request.form.get('height')
+        weight = float(request.form.get('weight'))
+        height = float(request.form.get('height'))
 
-        bmi = float(weight) / float(height)**2
+        bmi = calc_bmi(weight, height)
 
     return render_template("index.html",
                            name=name,
                            bmi=bmi)
+
+
+def calc_bmi(weight, height):
+    return round((weight / height ** 2), 2)
 
 
 if __name__ == '__main__':
